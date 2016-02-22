d437dd6b873d21eb4c89575481634d1e082df81a
Hendrik Kurniawan <hendrik.kurniawan@rtl.nl>
2016-02-23 00:02:43 +0100

    using stateless components :smirk:

--- components/HelloMessage/index.js
+++ components/HelloMessage/index.js
@@ -4,14 +4,8 @@ if (process.env.BROWSER) {
     require('./hello.scss');
 }
 
-class HelloMessage extends React.Component {
-    render() {  
-        return (
-            <div className='hello-container'>
+export default function HelloMessage() {
+    return  <div className='hello-container'>
                 <p>Hello world</p>
             </div>
-        )
-    }
 }
-
-module.exports = HelloMessage
