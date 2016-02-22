9e87f6d79282e887ed09d1de13febc7903c1fa33
Hendrik Kurniawan <hendrik.kurniawan@rtl.nl>
2016-02-22 23:07:22 +0100

    redux ssr combined with react router :heart_eyes:

--- client.js
+++ client.js
@@ -1,18 +1,34 @@
 import React from 'react'
-import { match, browserHistory, Router } from 'react-router'
 import { render } from 'react-dom'
 import routes from './routes/Index'
+
+import { Provider } from 'react-redux'
+import { Router, browserHistory, match } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { configureStore } from './stores/store'
 
 if (process.env.BROWSER) {
     require('./styles/manifest.scss');
 }
-
 const { pathname, search, hash } = window.location
 const location = `${pathname}${search}${hash}`
 
-match({ routes, location, browserHistory }, (error, redirectLocation, renderProps) => {
+const store = configureStore(browserHistory, window.__initialState__)
+const history = syncHistoryWithStore(browserHistory, store)
+
+// This, if work well, would be the correct way to render on client
+// render(
+//   <Provider store={store}>
+//     <Router history={history} routes={routes} />
+//   </Provider>,
+//   document.getElementById('app')
+// )
+
+match({ routes, location, history }, (error, redirectLocation, renderProps) => {
     render(
-        <Router {...renderProps} />, 
+        <Provider store={store}>
+            <Router {...renderProps} />
+        </Provider>, 
         document.getElementById('app')
     )
 })
